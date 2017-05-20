2fa0c85ffd5b109e6f441a046a013b07142650b8
lulu9091 <lulu9091@gmail.com>
2017-05-20 19:20:59 -0400

    1

---
+++ hw2/leapyear.test.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+public static boolean isLeapYear(int year) {
+    System.in.
+
+
+
+
+
+        if (year%4!=0 || year%400!=0){
+           if (year%100==true){
+               returen false;
+           }
+         else {
+              return true;
+         }  
+        }                           // replace this line with your solution
+}
